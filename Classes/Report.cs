f3580b0f6b1ea5c284a67583d0d2a711679a08b6
ZafarUrakov <goliburakov535@gmail.com>
2023-08-30 22:15:45 -0700

    CODE RUB: Refactor Code

---
+++ Classes/Report.cs
@@ -0,0 +1,58 @@
+using GeometryGuru.Interfaces;
+
+namespace GeometryGuru.Classes;
+
+public class Report : IReport
+
+{
+    public int FormatExceptionMessage()
+    {
+
+        int firstOption = 0;
+        try
+        {
+            firstOption = int.Parse(Console.ReadLine());
+            Console.Clear();
+        }
+        catch (FormatException formatException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The input is not in a correct format");
+            Console.ResetColor();
+        }
+        catch (OverflowException overflowException)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Value is too large or too small, take a look data type.");
+            Console.ResetColor();
+
+        }
+        catch (Exception exception)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(exception.Message);
+            Console.ResetColor();
+
+        }
+
+        return firstOption;
+    }
+
+    public void ResultProgress()
+    {
+        Console.WriteLine("\nData progressing..");
+    }
+
+    public void DefaultProgress()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("There is mo such choice, try again."); 
+        Console.ResetColor();
+    }
+
+    public void Exit()
+    {
+        Console.WriteLine("Thanks for using the app..");
+    }
+}
